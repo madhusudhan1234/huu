9e555180df1301751c24c23a8dde92aa8814577f
Madhu Sudhan <madhu.subedi@monotype.com>
2018-04-23 22:26:50 +0545

    Question is Created

--- config/routes.rb
+++ config/routes.rb
@@ -6,10 +6,7 @@ Rails.application.routes.draw do
 
   get 'home/index'
 
-  post '/questions' => 'home#temp'
   post '/answers' => 'home#temp'
-
-  get '/questions/:id' => 'home#question'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
