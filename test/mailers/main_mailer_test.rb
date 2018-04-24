b52f7d88815498724da949275e941c327bde563e
Madhu Sudhan <madhu.subedi@monotype.com>
2018-04-24 18:35:20 +0545

    Test Drive Development and Email Previews

--- test/mailers/main_mailer_test.rb
+++ test/mailers/main_mailer_test.rb
@@ -2,11 +2,15 @@ require 'test_helper'
 
 class MainMailerTest < ActionMailer::TestCase
   test "notify_question_author" do
-    mail = MainMailer.notify_question_author
+    question = Question.create email: 'author@question.com', body: 'a test question'
+    answer = Answer.create email: 'author@answer.com', body: 'a test answer'
+
+    question.answers << answer
+    mail = MainMailer.notify_question_author(answer)
     assert_equal "New Answer for your question", mail.subject
-    assert_equal ["to@example.org"], mail.to
-    assert_equal ["from@example.com"], mail.from
-    assert_match "Hi", mail.body.encoded
+    assert_equal [question.email], mail.to
+    assert_equal [answer.email], mail.from
+    assert_match answer.body, mail.body.encoded
   end
 
 end
